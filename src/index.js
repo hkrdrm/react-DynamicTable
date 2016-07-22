4b1f533be848a1e70231e2c8d67fd0b094bd122f
tspiers <tspiers@telapexinc.com>
2016-07-22 11:49:26 -0500

    hello

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import 'core-js/fn/object/assign'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/Main'
-import DynamicTable from './components/DynamicTable'
 
-export default DynamicTable
+require('./components/DynamicTable').default
+exports.default = DynamicTable
 /*
 // Render the main component into the dom
 ReactDOM.render(
