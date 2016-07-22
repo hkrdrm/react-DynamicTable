338c47a272b3ebcf9a2e0601edd3e7a49fa61cae
tspiers <tspiers@telapexinc.com>
2016-07-22 11:28:06 -0500

    changed index

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './components/Main';
 
+module.exports = require('./components/DynamicTable')
+
+/*
 // Render the main component into the dom
 ReactDOM.render(
   <App />,
   document.getElementById('app')
  );
+*/
