61a68da46ba7a2850217d30adda6ffad94dd9e95
tspiers <tspiers@telapexinc.com>
2016-07-22 11:53:21 -0500

    test

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import 'core-js/fn/object/assign'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/Main'
+import DynamicTable from './components/DynamicTable'
 
 require('./components/DynamicTable').default
 exports.default = DynamicTable
